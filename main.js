b95a8eb92b9a674f16bbb4c948442d995ef02ffd
trumvang999 <79635167+trumvang999@users.noreply.github.com>
2025-10-01 22:54:06 +0700

    Update main.js

--- main.js
+++ main.js
@@ -162,7 +162,19 @@
   }
 
   // ===================== Toggle History / Purchase =====================
+  window.toggleHistory = function() { 
+    const box = document.getElementById("history-section"); 
+    if(!box) return;
+    box.style.display = box.style.display==="none"?"block":"none"; 
+    if(box.style.display==="block") renderHistory(); 
+  }
 
+  window.togglePurchase = function() {
+    const box = document.getElementById("purchase-history-box");
+    if(!box) return;
+    box.style.display = box.style.display==="none"?"block":"none";
+    if(box.style.display==="block") window.loadHistoryTable();
+  }
   window.loadHistoryTable = async function() {
     const username = localStorage.getItem("currentUser");
     const password = localStorage.getItem("currentPass");
