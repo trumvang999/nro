246d9780052b819e78aeaecd6a30c62c1d0169e5
trumvang999 <79635167+trumvang999@users.noreply.github.com>
2025-11-13 21:00:18 +0700

    Update gifts.js

--- gifts.js
+++ gifts.js
@@ -1,6 +1,7 @@
 const btn    = document.getElementById("claim-gift-btn");
 const msgBox = document.getElementById("gift-message");
 const wrapper = document.getElementById("gift-container");
+const user = localstorage.getItem("currentUser");
 
 window.addEventListener("DOMContentLoaded", () => {
   if (!btn || !msgBox) return;
@@ -10,9 +11,15 @@ window.addEventListener("DOMContentLoaded", () => {
 
   // Hoặc giữ nút nhưng click sẽ báo bảo trì
   btn.addEventListener("click", () => {
+    if (!user){
+          msgBox.innerText = "Vui lòng đăng nhập để nhận quà!";
+    msgBox.style.display = "block";
+          setTimeout(() => msgBox.style.display = "none", 2000); // 3s biến mất
+    }); }
+    else{
     msgBox.innerText = "Chức năng nhận quà đang bảo trì!";
     msgBox.style.display = "block";
         btn.style.display = "none";
     setTimeout(() => msgBox.style.display = "none", 3000); // 3s biến mất
-  });
+  });}
 });
